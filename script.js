75f627d6c971e879b9819fe9584967421f42f170
palraiz <palraiz.studio@gmail.com>
2021-07-20 14:07:19 +0300

    Подгрузка LocalStorage
    Добавлена подгрузка с хранилища при запуске странички

--- script.js
+++ script.js
@@ -1,97 +1,53 @@
-//selecting dom elements for manipulation
-var input = document.querySelector("input[type = 'taskInput']");
-var ul = document.querySelector("ul");
-var container = document.querySelector("div");
-var lists = document.querySelectorAll("li");
-var spans = document.getElementsByTagName("span");
-var pencil = document.querySelector("#pencil");
-var saveBtn = document.querySelector(".taskSave");
-var clearBtn = document.querySelector(".taskClear");
-var tipsBtn = document.querySelector(".tipBtn");
-var closeBtn = document.querySelector(".closeBtn");
-var overlay = document.getElementById("overlay")
+if (localStorage.removeItem("Key") === undefined) {
+    let tasks = [];
+    tasks[0] = [];
+    tasks.push ({
+        number: 1,
+        task: 'Прокинутися',
+        checked: false,
+        delete: false
+    });
+    tasks.push ({
+        number: 2,
+        task: 'Піти вбитися',
+        checked: false,
+        delete: false
+    });
 
-
-//function to delete task if delete span is clicked.
-function deleteTask(){
-    for(let span of spans){
-        span.addEventListener ("click",function (){
-            span.parentElement.remove();
-            event.stopPropagation();
-        });
+    let outTask = document.getElementById('task');
+    for (let i = 0; i < tasks.length; i++  ) {
+        text = '<li> <span class = "taskText"> </span>' + tasks[i].number + '. ' + tasks[i].task + '<span class = "taskTrash"> <i class = "fas fa-trash-alt"> </i> </span> </li> <br>';
+        outTask.innerHTML = text;
+        // работает, но при этом не работает...
     }
+    
+} else {
+    let tasks = localStorage.removeItem("Key");
 }
 
-//function to load task if list is found in local storage.
-function loadTask(){
-    if(localStorage.getItem('List')){
-        ul.innerHTML = localStorage.getItem('List');
-        deleteTask();
-    }
-}
+//console.log(tasks[0].task);
 
-//event listener for input to add new task to the list.
-input.addEventListener("keypress",function(keyPressed){
-    if(keyPressed.which === 13){
-        //creating lists and span when enter is clicked
-        var li = document.createElement("li");
-        var spanElement = document.createElement("span");
-        var icon = document.createElement("i");
 
-        var newTask = this.value;
-        this.value = " " ;
+$(document).keyup(function(e) {
+    if (e.key === "Enter" || e.keyCode === 13) {
+        let text = $("input").val();
+        //input.value() = ''; почему-то не работать
+        tasks.push ({
+            number: tasks.length,
+            task: text,
+            checked: false,
+            delete: false
+        })
 
-        icon.classList.add('fas', 'fa-trash-alt');
-        spanElement.append(icon);
-        ul.appendChild(li).append(spanElement,newTask);
+        for (let i = 0; i < tasks.length; i++)
+            console.log(tasks[i].task)
 
-        deleteTask();
-
-    }
-
-});
-
-// event listener to linethrough list if clicked
-ul.addEventListener('click', function(ev) {
-        if (ev.target.tagName === 'li') {
-            ev.target.classList.toggle('checked');
+        let outTask = document.getElementById('task');
+        for (let i = 0; i < tasks.length; i++  ) {
+                let num = Number(tasks[i].number)+1;
+                text = '<li> <span class = "taskText"> </span>' + num + '. ' + tasks[i].task + '<span class = "taskTrash"> <i class = "fas fa-trash-alt"> </i> </span> </li> <br>';
+            outTask.innerHTML = text;
+                // работает, но при этом не работает...
         }
-    },false
-);
-
-//hide input box,when pencil icon is clicked
-pencil.addEventListener('click', function(){
-    input.classList.toggle('display');
+    }
 });
-
-
-
-//save todolist state so user can access it later
-saveBtn.addEventListener('click',function(){
-    localStorage.setItem('List',ul.innerHTML );
-
-});
-
-//clear all task when clear button is clicked
-clearBtn.addEventListener('click', function(){
-    ul.innerHTML= "";
-    localStorage.removeItem('List',ul.innerHTML );
-});
-
-//display overlay when tips btn is clicked
-tipsBtn.addEventListener("click",function(){
-    overlay.style.height = "100%";
-});
-
-//close overlay when close btn is clicked
-closeBtn.addEventListener("click",function(e){
-    e.preventDefault;
-    overlay.style.height = "0";
-
-})
-
-//delete listList
-deleteTask();
-
-//load task
-loadTask();
